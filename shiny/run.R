a2520c8fe8ee98b9af144f133406bc58c21b5e58
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2021-11-10 16:45:43 +0100

    update run script

---
+++ shiny/run.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+requireNamespace("shiny")
+
+main <- function(args) {
+  if (length(args) < 1 || length(args) > 3) {
+    cat("Usage:\n")
+    cat("Rscript run.R [<appDir>] [<host>] [<port>]\n")
+    return(1)
+  }
+
+  appDir <- ifelse(length(args) > 0, args[1], ".")
+  host <- ifelse(length(args) > 1, args[2], "127.0.0.1")
+  port <- ifelse(length(args) > 2, as.numeric(args[3]), 8000)
+
+  cat("starting server in '", appDir, "', binding to ", host, ":", port, "\n", sep="")
+
+  return(shiny::runApp(shiny::shinyAppDir(appDir, options = list(port = port, host = host))))
+}
+
+
+quit(status = main(commandArgs(TRUE)))
