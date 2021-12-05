cc924c9156c631f5d057769943dbbe7dd6674005
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2021-12-05 18:51:01 +0100

    tweak run.R

--- shiny/run.R
+++ shiny/run.R
@@ -9,12 +9,12 @@ main <- function(args) {
   }
 
   app_dir <- args[1]
-  host <- ifelse(length(args) > 1, args[2], "127.0.0.1")
-  port <- ifelse(length(args) > 2, as.numeric(args[3]), 8000)
+  host <- ifelse(length(args) > 1, args[2], "0.0.0.0")
+  port <- ifelse(length(args) > 2, as.numeric(args[3]), 3838)
 
   cat("starting server in '", app_dir, "', binding to ", host, ":", port, "\n", sep = "")
 
-  return(shiny::runApp(shiny::shinyAppDir(app_dir, options = list(port = port, host = host))))
+  return(shiny::runApp(app_dir, port = port, host = host))
 }
 
 
