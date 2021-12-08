34bf08903704cd02765d5aa3b19f965c21d188eb
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2021-12-08 13:55:45 +0100

    generalize annotation parsing

--- pipeline/annotation.py
+++ pipeline/annotation.py
@@ -3,115 +3,160 @@ from typing import Dict, Optional, NamedTuple
 import ruamel.yaml
 
 
-class _Field(NamedTuple):
+class AnnotationError(Exception):
+    pass
+
+
+class Field(NamedTuple):
     name: str
     type: str
     help: str
     split_by: Optional[str]
 
 
-def _pop_str_list(data, name, key):
-    values = data.pop(key, [])
-    if not values:
-        raise AnnotationError(f"No {key} for plugin {name!r}")
-    elif not isinstance(values, list):
-        raise AnnotationError(f"{key} for plugin {name!r} are not a list")
+def _str_list(value):
+    """Require that a value is List[str]"""
+    if not isinstance(value, list):
+        raise AnnotationError("not a list")
+    elif any(not isinstance(it, str) for it in value):
+        raise AnnotationError("non-string values found")
 
-    if any(not isinstance(value, str) for value in values):
-        raise AnnotationError(f"{key} for plugin {name!r} contains non-strings")
-
-    return tuple(values)
+    return value
 
 
-def _pop_fields(data, name, default_type) -> Dict[str, _Field]:
-    values = data.pop("Fields", {})
-    if not values:
-        raise AnnotationError(f"No Fields for plugin {name!r}")
-    elif not isinstance(values, dict):
+def _str_dict(value):
+    """Require that a value is Dict[str, str]"""
+    if not isinstance(value, dict):
+        raise AnnotationError("not a dict")
+    elif not all(isinstance(it, str) for it in value):
+        raise AnnotationError("non-string keys found")
+    elif not all(isinstance(it, str) for it in value.values()):
+        raise AnnotationError("non-string values found")
+
+    return value
+
+
+def _truthy(type):
+    """Wraper around _str_list/_str_dict to also require a truthy (non-empty) value"""
+
+    def _is_truthy(value):
+        value = type(value)
+        if not value:
+            raise AnnotationError("no value")
+
+        return value
+
+    return _is_truthy
+
+
+def _parse(layout, name, data):
+    if not isinstance(name, str):
+        raise AnnotationError(f"Name {name!r} is not a string")
+    elif not isinstance(data, dict):
+        raise AnnotationError(f"{name!r} settings are not a dict")
+
+    output = {}
+    for key, info in layout.items():
+        value = data.pop(key, None)
+        if value is None:
+            if "default" not in info:
+                raise AnnotationError(f"no {key} specified for {name}")
+
+            # No validation of default value to allow e.g. None
+            output[key] = info["default"]
+            continue
+
+        vtype = info["type"]
+        if type(vtype) is type and not isinstance(value, vtype):
+            raise AnnotationError(f"{key} for {name} is not a {vtype.__name__}")
+
+        try:
+            output[key] = vtype(value)
+        except AnnotationError as error:
+            raise AnnotationError(f"invalid {key} for {name}: {error}")
+
+    if data:
+        raise AnnotationError(f"unexpected settings in {name!r}: {data!r}")
+
+    return output
+
+
+def combine_variables(variables, user_variables):
+    """Combine user Variables with built-in variables"""
+    user_variables = dict(user_variables)
+    user_variables.update(variables)
+
+    return {
+        key: str(value).format(**user_variables)
+        for key, value in user_variables.items()
+    }
+
+
+def apply_variables(variables, values):
+    for value in values:
+        yield value.format(**variables)
+
+
+def _parse_fields(data, name) -> Dict[str, Field]:
+    default_type = data.pop("FieldType")
+    data = data.pop("Fields")
+
+    if data is None:
+        return {}
+    elif not isinstance(data, dict):
         raise AnnotationError(f"Fields for plugin {name!r} are not a dict")
 
-    for key, value in values.items():
+    layout = {
+        "Name": {"type": str, "default": None},
+        "Help": {"type": str, "default": ""},
+        "FieldType": {"type": str, "default": default_type},
+        "Split-by": {"type": str, "default": None},
+    }
+
+    for key, value in data.items():
         if not isinstance(key, str):
             raise AnnotationError(f"Fields for plugin {name!r} has non-str key {key!r}")
-        elif not isinstance(value, dict):
-            value = {"Name": value, "Split-by": None}
+        elif value is None:
+            value = {"Name": None}
 
-        name = value.pop("Name", None)
-        if not (isinstance(name, str) or name is None):
-            raise AnnotationError(f"Field {name!r} for plugin {name!r} is not str")
-        elif not (name or name is None):
-            raise AnnotationError(f"No name for Field {key!r} for plugin {name!r}")
+        value = _parse(layout, f"Field {key} for {name}", value)
+        field_type = value["FieldType"]
+        if field_type not in ("str", "int", "float"):
+            raise AnnotationError(f"Bad type {field_type!r} in {key!r} for {name!r}")
 
-        split_by = value.pop("Split-by", None)
-        if not (isinstance(split_by, str) or split_by is None):
-            raise AnnotationError(
-                f"Bad Split-By {split_by} for Field {key!r} for plugin {name!r}"
-            )
-
-        help = value.pop("Help", None)
-        if help is None:
-            help = ""
-        elif not isinstance(help, str):
-            raise AnnotationError(f"Help {name!r} for plugin {name!r} is not str")
-
-        type = value.pop("FieldType", default_type)
-        if isinstance(type, str):
-            type = type.lower()
-
-        if type not in ("str", "int", "float"):
-            raise AnnotationError(
-                f"Bad Type {type} for Field {key!r} for plugin {name!r}"
-            )
-
-        if value:
-            raise AnnotationError(
-                f"Unexpected keys {tuple(value)} for Field {key!r} for plugin {name!r}"
-            )
-
-        values[key] = _Field(
-            name=name,
-            type=type,
-            help=help,
-            split_by=split_by,
+        data[key] = Field(
+            name=value["Name"],
+            help=value["Help"],
+            type=value["FieldType"],
+            split_by=value["Split-by"],
         )
 
-    return values
+    return data
 
 
-def _replace_vars(values, variables):
-    result = []
-    for value in values:
-        result.append(value.format(**variables))
-
-    return tuple(result)
-
-
-class AnnotationError(Exception):
-    pass
+_PLUGIN_LAYOUT = {
+    "Rank": {"type": int, "default": 0},
+    "Files": {"type": _truthy(_str_list)},
+    "Parameters": {"type": _truthy(_str_list)},
+    "Variables": {"type": _str_dict, "default": {}},
+    "FieldType": {"type": str, "default": "str"},
+    "Fields": {"type": lambda it: it, "default": {}},
+}
 
 
 class Plugin:
     def __init__(self, name, data, variables):
-        if not isinstance(name, str):
-            raise AnnotationError(f"Plugin name {name!r} is not a string")
-        elif data is None:
-            raise AnnotationError(f"No settings for plugin {name!r}")
-        elif not isinstance(data, dict):
-            raise AnnotationError(f"Plugin {name!r} settings are not a dict")
+        data = _parse(_PLUGIN_LAYOUT, name, data)
 
-        user_variables = data.pop("Variables", {})
-        if not isinstance(user_variables, dict):
-            raise AnnotationError(f"Variables for plugin {name!r} not a dict")
-        variables = dict(variables)
-        for key, value in user_variables.items():
-            variables[key] = value.format(**variables)
+        variables = combine_variables(variables, data.pop("Variables"))
 
+        self.rank = data.pop("Rank")
         self.name = name
-        self.files = _replace_vars(_pop_str_list(data, name, "Files"), variables)
-        self._params = _replace_vars(_pop_str_list(data, name, "Parameters"), variables)
+        self.files = apply_variables(variables, data.pop("Files"))
+        self._params = apply_variables(variables, data.pop("Parameters"))
+        self.fields = _parse_fields(data, name)
 
-        if data:
-            raise AnnotationError(f"Unexpected settings in plugin {name!r}: {data!r}")
+        assert not data, data
 
     @property
     def params(self):
@@ -121,32 +166,30 @@ class Plugin:
         return ["--plugin", ",".join(params)]
 
 
+_CUSTOM_LAYOUT = {
+    "Rank": {"type": int, "default": 0},
+    "File": {"type": str},
+    "Variables": {"type": _str_dict, "default": {}},
+    "FieldType": {"type": str, "default": "str"},
+    "Fields": {"type": lambda it: it, "default": {}},
+}
+
+
 class Custom:
     def __init__(self, name, data, variables, type):
-        if not isinstance(name, str):
-            raise AnnotationError(f"Plugin name {name!r} is not a string")
-        elif data is None:
-            raise AnnotationError(f"No settings for plugin {name!r}")
-        elif not isinstance(data, dict):
-            raise AnnotationError(f"Plugin {name!r} settings are not a dict")
-        elif type not in ("vcf", "bed"):
+        if type not in ("vcf", "bed"):
             raise AnnotationError(f"invalid custom annotation type {type!r}")
 
-        file_ = data.pop("File", None)
-        if not file_:
-            raise AnnotationError(f"No File specified for custom {name!r}")
-        elif not isinstance(file_, str):
-            raise AnnotationError(f"{name!r} File is not a string: {self._file!r}")
+        data = _parse(_CUSTOM_LAYOUT, name, data)
+        variables = combine_variables(variables, data.pop("Variables"))
 
+        self.rank = data.pop("Rank")
         self.name = name
         self._type = type
-        self._file = file_.format(**variables)
+        self._file = data.pop("File").format(**variables)
+        self.fields = _parse_fields(data, name)
 
-        default_type = data.pop("FieldType", "str")
-        self.fields = _pop_fields(data, name, default_type) if type == "vcf" else {}
-
-        if data:
-            raise AnnotationError(f"Unexpected settings in plugin {name!r}: {data!r}")
+        assert not data, data
 
     @property
     def files(self):
@@ -155,7 +198,9 @@ class Custom:
     @property
     def params(self):
         params = [self._file, self.name, self._type, "overlap", "0"]
-        params.extend(self.fields)
+        for name in self.fields:
+            if not (name.startswith(":") and name.endswith(":")):
+                params.append(name)
 
         return ["--custom", ",".join(params)]
 
@@ -181,5 +226,8 @@ def load_annotations(filepaths, variables=None):
                 value = Custom(name, settings, variables, type="bed")
             else:
                 raise AnnotationError(f"Unknown annotation type {type!r} for {name!r}")
+
+            # Ensure that ordering is stable in relation to annotation files
+            value.rank = (value.rank, idx)
 
             yield value
